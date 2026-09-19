e54910aa36c1cb7f9ae0766e514b6023238fe000
Tim Daw <tim.daw@gmail.com>
2026-09-19 07:58:14 -0700

    App entry: gunzip base64 chunks into locked overview viewer

--- vendor/locked_overview_app.js
+++ vendor/locked_overview_app.js
@@ -1,13 +1,9 @@
-/** Assembles plain parts into the locked overview app module. */
+/** Gunzips base64 chunks into the locked overview app. */
 const base = new URL('.', import.meta.url);
-const n = 5;
-const texts = await Promise.all([...Array(n).keys()].map(i =>
-  fetch(new URL('./locked_overview_app.part' + i + '.js.txt', base)).then(r => {
-    if (!r.ok) throw new Error('missing part ' + i + ' HTTP ' + r.status);
-    return r.text();
-  })
-));
-let code = texts.join('');
+const parts = await Promise.all([0,1,2].map(i => fetch(new URL('./locked_overview_app.b64.'+i+'.txt', base)).then(r=>r.text())));
+const bin = Uint8Array.from(atob(parts.join('')), c => c.charCodeAt(0));
+const ds = new DecompressionStream('gzip');
+let code = await new Response(new Blob([bin]).stream().pipeThrough(ds)).text();
 code = code.replace(/from\s+['"]\.\/osgb_wgs84\.js['"]/g, "from '" + new URL('./osgb_wgs84.js', base).href + "'");
 code = code.replace(/from\s+['"]three['"]/g, "from '" + new URL('./three.module.js', base).href + "'");
 code = code.replace(/from\s+['"]three\/addons\/OrbitControls\.js['"]/g, "from '" + new URL('./OrbitControls.js', base).href + "'");

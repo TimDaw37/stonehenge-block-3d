3ff08ab1f86294f5fb1d239f0110f993443a69ad
Tim Daw <tim.daw@gmail.com>
2026-09-19 07:55:59 -0700

    Add locked_overview_app.js assembler (loads plain .part*.js.txt)

---
+++ vendor/locked_overview_app.js
@@ -0,0 +1,13 @@
+/** Assembles plain parts into the locked overview app module. */
+const base = new URL('.', import.meta.url);
+const texts = await Promise.all([0,1,2].map(i =>
+  fetch(new URL('./locked_overview_app.part' + i + '.js.txt', base)).then(r => {
+    if (!r.ok) throw new Error('missing part ' + i);
+    return r.text();
+  })
+));
+let code = texts.join('');
+code = code.replace(/from\s+['"]\.\/osgb_wgs84\.js['"]/g, "from '" + new URL('./osgb_wgs84.js', base).href + "'");
+code = code.replace(/from\s+['"]three['"]/g, "from '" + new URL('./three.module.js', base).href + "'");
+code = code.replace(/from\s+['"]three\/addons\/OrbitControls\.js['"]/g, "from '" + new URL('./OrbitControls.js', base).href + "'");
+await import(URL.createObjectURL(new Blob([code], { type: 'text/javascript' })));
